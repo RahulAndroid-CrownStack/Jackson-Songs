b4b717318e48f4b617de43778229cba47a40b78d
CODER_BY_CHANCE <rahulsingh11490@gmail.com>
2021-06-08 13:43:15 +0530

    RxJava dependency addedRahulSingh-android

--- app/src/main/java/com/coderbychance/jecksonsong/ui/song_list/SongViewModel.kt
+++ app/src/main/java/com/coderbychance/jecksonsong/ui/song_list/SongViewModel.kt
@@ -14,6 +14,7 @@ import io.reactivex.schedulers.Schedulers
 
 class SongViewModel @ViewModelInject constructor(private val songRepository: SongRepository) :
     ViewModel() {
+    //aa
 
     var songsData: MutableLiveData<SongResponse> = MutableLiveData<SongResponse>()
 
